079433892006c722b67d12361067b0d5178f24f3
wangkaihui <hpuwang@gmail.com>
2026-04-17 22:05:48 +0800

    init

--- admin-ui/packages/icons/src/iconify/index.ts
+++ admin-ui/packages/icons/src/iconify/index.ts
@@ -1,4 +1,14 @@
-import { createIconifyIcon } from '@vben-core/icons';
+import { addCollection, createIconifyIcon } from '@vben-core/icons';
+
+// 导入本地图标集
+import lucideIcons from '@iconify-json/lucide/icons.json';
+import mdiIcons from '@iconify-json/mdi/icons.json';
+import carbonIcons from '@iconify-json/carbon/icons.json';
+
+// 注册本地图标集
+addCollection(lucideIcons);
+addCollection(mdiIcons);
+addCollection(carbonIcons);
 
 export * from '@vben-core/icons';
 
